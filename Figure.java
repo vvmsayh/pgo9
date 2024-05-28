f6094ee0e27176768147a90f4311a8fa9be042e0
vvmsayh <vvmsay@gmail.com>
2024-05-28 11:15:34 +0200

    task1

---
+++ Figure.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
+
+interface Figure {
+    double getPerimeter();
+    double getArea();
+}
+
+class Circle implements Figure {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
+class Parallelogram implements Figure {
+    private double baseSide;
+    private double side;
+    private double height;
+
+    public Parallelogram(double baseSide, double side, double height) {
+        this.baseSide = baseSide;
+        this.side = side;
+        this.height = height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (baseSide + side);
+    }
+
+    @Override
+    public double getArea() {
+        return baseSide * height;
+    }
+}
+class Rectangle implements Figure {
+    private double sideA;
+    private double sideB;
+
+    public Rectangle(double sideA, double sideB) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (sideA + sideB);
+    }
+
+    @Override
+    public double getArea() {
+        return sideA * sideB;
+    }
+}
+
---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        double radius = 3d;
+        Figure circle = new Circle(radius);
+
+        double baseSide = 10d;
+        double side = 5d;
+        double height = 8d;
+        Figure parallelogram = new Parallelogram(baseSide, side, height);
+
+        double sideA = 15d;
+        double sideB = 10d;
+        Figure rectangle = new Rectangle(sideA, sideB);
+
+        List<Figure> figures = new ArrayList<>(3);
+        figures.add(circle);
+        figures.add(parallelogram);
+        figures.add(rectangle);
+
+        for (Figure figure : figures) {
+            System.out.println("Area: " + figure.getArea() + " units.");
+            System.out.println("Perimeter: " + figure.getPerimeter() + " units.\n");
+        }
+    }
+}
